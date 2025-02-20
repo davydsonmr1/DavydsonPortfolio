08311c33a5b4ca328540ea26762cca0b6d2e6a15
davydsonmr1 <davydsonmaciel.dev@gmail.com>
2025-02-20 12:18:49 -0300

    -

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,38 +1,113 @@
-import React from 'react';
+import React, { useState } from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  NavLink,
+} from "react-router-dom";
 
 function Content() {
+  const [filter, setFilter] = useState("todos");
+
   const posts = [
     {
-      title: "Sistema de gerenciamento de Academias",
-      tec: "HTML, CSS, JAVASCRIPT",
-      link: "https://github.com/davydsonmr1/sistema-teste-academia"
+      title: "Calculadora de IMC",
+      tec: "React",
+      level: "Iniciante",
+      link: "https://github.com/davydsonmr1/Calculadora-IMC-React",
     },
     {
-      title: "Sistema de gerenciamento de estoque de uma padaria",
+      title: "Calculadora em Typescript",
+      tec: "Typescript",
+      level: "Iniciante",
+      link: "https://github.com/davydsonmr1/Calculadora-em-Typescript",
+    },
+    {
+      title: "Card de apresentação",
       tec: "HTML, CSS, JAVASCRIPT",
-      link: "https://github.com/davydsonmr1/Sistema-estoque-padaria"
+      level: "Iniciante",
+      link: "https://github.com/davydsonmr1/Card-Apresentacao",
+    },
+    {
+      title: "To-Do-List",
+      tec: "HTML, CSS, JAVASCRIPT",
+      level: "Iniciante",
+      link: "https://github.com/davydsonmr1/to-do-list",
+    },
+    {
+      title: "Jogo da memória",
+      tec: "HTML, CSS, JAVASCRIPT",
+      level: "Iniciante",
+      link: "https://github.com/davydsonmr1/memory-game",
+    },
+    {
+      title: "Cronograma de estudos com timer e gerador de perguntas",
+      tec: "HTML, CSS, JAVASCRIPT",
+      level: "Iniciante",
+      link: "https://github.com/davydsonmr1/Cronograma-Estudos",
+    },
+    {
+      title: "Sistema de tarefas Kanban",
+      tec: "HTML, CSS, JAVASCRIPT",
+      level: "Iniciante",
+      link: "https://github.com/davydsonmr1/SistemaTarefasKanban",
     },
     {
       title: "Dashboard de finanças pessoais",
       tec: "REACT , TAILWIND CSS",
-      link: "https://github.com/davydsonmr1/project-react-app-finan-as"
-    }
-    
+      level: "Intermediario",
+      link: "https://github.com/davydsonmr1/project-react-app-finan-as",
+    },
+    {
+      title: "Jogo da Velha",
+      tec: "Javascript",
+      level: "Intermediario",
+      link: "https://github.com/davydsonmr1/jogo-da-velha",
+    },
+    {
+      title: "Sistema de estoque de padaria",
+      tec: "Javascript",
+      level: "Intermediario",
+      link: "https://github.com/davydsonmr1/Sistema-estoque-padaria",
+    },
+    {
+      title: "Sistema de gerenciamento de academia",
+      tec: "Javascript",
+      level: "Intermediario",
+      link: "https://github.com/davydsonmr1/sistema-teste-academia",
+    },
+
   ];
+
+  const filteredPosts = posts.filter((post) => {
+    if (filter === "todos") return true;
+    return post.level.toLowerCase() === filter;
+  });
 
   return (
     <div className="content-section">
-      
-      
-      <h2>Projetos recentes</h2>
+      <div className="filter-h1">
+        <h2>Projetos recentes</h2>
+        <select
+          name="filter"
+          id="filter"
+          value={filter}
+          onChange={(e) => setFilter(e.target.value)}
+        >
+          <option value="todos">Todos</option>
+          <option value="iniciante">Iniciante</option>
+          <option value="intermediario">Intermediario</option>
+          <option value="avançado">Avançado</option>
+        </select>
+      </div>
       <div className="post-list">
-        {posts.map((post, index) => (
+        {filteredPosts.map((post, index) => (
           <article key={index} className="post-item">
             <h3>{post.title}</h3>
             <p>{post.tec}</p>
-            <a href={post.link} target='blank' className="permalink">Saiba mais</a>
-            
-            
+            <a href={post.link} target="blank" className="permalink">
+              Saiba mais
+            </a>
           </article>
         ))}
       </div>
